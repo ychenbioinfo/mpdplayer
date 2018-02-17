6d14a9d11d96d8db09fe6b0adad7489fc301871e
Alvin Chen <ychenbioinfo@gmail.com>
2018-02-16 22:44:34 -0500

    add more controls

--- mpdplayer/static/js/controller.js
+++ mpdplayer/static/js/controller.js
@@ -1,7 +1,14 @@
 $(document).ready(function () {
+    $('#btn-pause').hide();
+    $('#btn-repeat_one').hide();
+    $('#btn-repeat').css('color', 'gray');
+
     $('#btn-play').click(function (e) {
         // e.preventDefault();
         console.log("play button clicked!");
+        // $('#btn-play').css('color', 'red');
+        $('#btn-play').hide();
+        $('#btn-pause').show();
         $.ajax({
             type: 'POST',
             url: $SCRIPT_ROOT + '_play',
@@ -16,6 +23,8 @@ $(document).ready(function () {
     $('#btn-pause').click(function (e) {
         // e.preventDefault();
         console.log("play button clicked!");
+        $('#btn-pause').hide();
+        $('#btn-play').show();
         $.ajax({
             type: 'POST',
             url: $SCRIPT_ROOT + '_pause',
@@ -26,6 +35,88 @@ $(document).ready(function () {
             },
         });
     });
+
+    $('#btn-backward').click(function(e) {
+        $.ajax({
+            type: 'POST',
+            url: $SCRIPT_ROOT + '_backward',
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                console.log(data);
+            },
+        });
+    });
+
+    $('#btn-forward').click(function(e) {
+        $.ajax({
+            type: 'POST',
+            url: $SCRIPT_ROOT + '_forward',
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                console.log(data);
+            },
+        });
+    });
+
+    $('#btn-stop').click(function(e) {
+        $.ajax({
+            type: 'POST',
+            url: $SCRIPT_ROOT + '_stop',
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                console.log(data);
+            },
+        });
+    });
+
+    $('#btn-volume-up').click(function(e) {
+        $.ajax({
+            type: 'POST',
+            url: $SCRIPT_ROOT + '_vol_up',
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                console.log(data);
+            },
+        });
+    });
+
+    $('#btn-volume-down').click(function(e) {
+        $.ajax({
+            type: 'POST',
+            url: $SCRIPT_ROOT + '_vol_down',
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                console.log(data);
+            },
+        });
+    });
+
+    function update_status(data){
+        $('#name').text(data.name);
+    }
+
+    function load_status(){
+        $.ajax({
+            type: 'POST',
+            url: $SCRIPT_ROOT + '_status',
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                console.log(data);
+                update_status(data);
+            },
+        });
+    }
+
+    load_status();
+    setInterval(function(){
+        load_status();
+    }, 1000);
 });
 
 
--- mpdplayer/views.py
+++ mpdplayer/views.py
@@ -17,12 +17,46 @@ def run_command(command: list):
     return play_info
 
 
+def read_status(data):
+    status = dict()
+    if len(data) == 1:
+        status['mode'] == 'stop'
+        settings = read_settings(data[0])
+    else:
+        status['name'] = status[0]
+
+    status['volume'] = settings[0]
+    status['repeat'] = settings[1]
+    status['random'] = settings[2]
+    status['single'] = settings[3]
+
+
+def read_settings(settings):
+    data = settings.split()
+    volume = data[0][6:-1]
+    if data[1][7:] == "on":
+        repeat = 1
+    else:
+        repeat = 0
+
+    if data[2][7:] == "on":
+        random = 1
+    else:
+        random = 0
+
+    if data[3][7:] == "on":
+        single = 1
+    else:
+        single = 0
+
+    return [volume, repeat, random, single]
+
+
 @app.route('/_play', methods=['POST'])
 def play_music():
     if request.method == 'POST':
-        command = ['mpc', 'play']
-        play_info = run_command(command)
-        return jsonify(play_info)
+        status = run_command(['mpc', 'play'])
+        return jsonify(status)
 
 
 @app.route('/_pause', methods=['POST'])
@@ -30,6 +64,13 @@ def pause_music():
     if request.method == 'POST':
         p = subprocess.Popen(['mpc', 'pause'])
         return jsonify({'message': 'success'})
+
+
+@app.route('/_status', methods=['POST'])
+def get_status():
+    if request.method == 'POST':
+        status = run_command(['mpc', 'status'])
+        return jsonify(status)
 
 
 @app.route('/')
